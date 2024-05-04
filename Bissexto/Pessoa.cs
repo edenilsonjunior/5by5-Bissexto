36f76a34e25ca56c1809ab020467416555346807
Edenilson <edenilsonju10@gmail.com>
2024-05-03 23:09:27 -0300

    Add project files.

---
+++ Bissexto/Pessoa.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bissexto
+{
+    internal class Pessoa
+    {
+        string nome;
+        int idade;
+        string profissao;
+
+        public Pessoa(string nome, int idade, string profissao)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.profissao = profissao;
+        }
+
+        public string getNome()
+        {
+            return nome;
+        }
+
+        public int getIdade()
+        {
+            return idade;
+        }
+
+        public string GetProfissao()
+        {
+            return profissao;
+        }
+
+        public void SetNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public void SetIdade(int idade)
+        {
+            this.idade = idade;
+        }
+
+        public void SetProfissao(string profissao)
+        {
+            this.profissao = profissao;
+        }
+
+        public int GetIdadeEmBissexto()
+        {
+       
+            int anoAtual = DateTime.Now.Year;
+            int anoNascimento = anoAtual - idade;
+            int idadeBissexto = 0;
+            
+            // Vai percorrer desde o ano de nascimento até o ano atual
+            for(int ano = anoNascimento; ano <= anoAtual; ano++)
+            {
+                // Metodo da classe DateTime que verifica se um ano é bissexto(leap year)
+                if (DateTime.IsLeapYear(ano))
+                {
+                    idadeBissexto++;
+                }
+            }
+            return idadeBissexto;
+        }
+
+        public override string? ToString()
+        {
+            return $"Nome: {nome}\nProfissao: {profissao}\nIdade: {idade}";
+        }
+    }
+}
---
+++ Bissexto/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Bissexto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p;
+
+            Console.WriteLine("=====Idade em bissextos=====");
+            
+            Console.Write("Digite o nome: ");
+            string? nome = Console.ReadLine();
+
+            bool resultadoTry;
+            int idade;
+            do
+            {
+                Console.Write("Digite a idade: ");
+                resultadoTry = int.TryParse(Console.ReadLine(), out idade);
+
+                if (!resultadoTry)
+                    Console.WriteLine("Voce deve digitar um numero!");
+                else if(idade < 0)
+                    Console.WriteLine("Voce deve digitar uma idade maior que zero!");
+                
+            } while (!resultadoTry || idade < 0);
+            
+            Console.Write("Digite a profissao: ");
+            string? profissao = Console.ReadLine();
+
+            p = new Pessoa(nome, idade, profissao);
+
+            Console.WriteLine("=====================");
+            Console.WriteLine(p.ToString());
+            Console.WriteLine($"Idade em ano bissextos: {p.GetIdadeEmBissexto()}");
+        }
+    }
+}
